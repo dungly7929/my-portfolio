e11b09d2b21d73d19698ff543560af0fb4f4b8a1
Danny Ly <danks76429@gmail.com>
2023-03-16 20:15:26 -0400

    initial commit

---
+++ src/assets/index.js
@@ -0,0 +1,23 @@
+import cssImg from './css.png';
+import githubImg from './github.png';
+import htmlImg from './html.png';
+import javascriptImg from './javascript.png';
+import tailwindImg from './tailwind.png';
+import reactImg from './react.png';
+import androidImg from './android.png';
+import javaImg from './java.png';
+import reduxImg from './redux.png';
+import bootstrapImg from './bootstrap.png';
+
+export {
+    cssImg,
+    githubImg,
+    htmlImg,
+    javascriptImg,
+    tailwindImg,
+    reactImg,
+    androidImg,
+    javaImg,
+    reduxImg,
+    bootstrapImg,
+}
