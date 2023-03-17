a1f2adb27b7a33863c0ac922ea79d8ac8d57de4a
Danny Ly <danks76429@gmail.com>
2023-03-16 20:14:15 -0400

    adding png resource to projeact commit

---
+++ src/assets/portfolio/index.js
@@ -0,0 +1,19 @@
+import musicImg from './music.jpg';
+import heroImage from './heroImage.jpg';
+import installNode from './installNode.jpg';
+import navBar from './navbar.jpg';
+import reactParallax from './reactParallax.jpg';
+import reactSmooth from './reactSmooth.jpg';
+import reactWeather from './reactWeather.jpg';
+import useStateP from './usestate.jpg';
+
+export {
+    musicImg,
+    heroImage,
+    installNode,
+    navBar,
+    reactParallax,
+    reactSmooth,
+    reactWeather,
+    useStateP,
+}
