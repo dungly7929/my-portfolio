d21cf545373d9400213e5993389d779312f08154
Danny Ly <danks76429@gmail.com>
2023-03-16 20:16:04 -0400

    navbar page commit

---
+++ src/components/index.js
@@ -0,0 +1,17 @@
+import Home from './Home';
+import NavBar from './NavBar';
+import SocialLinks from './SocialLinks';
+import About from './About';
+import Portfolio from './Portfolio';
+import Experience from './Experience';
+import Contact from './Contact';
+
+export {
+    Home,
+    NavBar,
+    SocialLinks,
+    About,
+    Portfolio,
+    Experience,
+    Contact,
+}
